dcff9b3a0d46f328260dd590255f2dbdb8a937d1
Aswin-1121 <75619250+Aswin-1121@users.noreply.github.com>
2023-12-02 17:27:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,148 +1,43 @@
 document.addEventListener("DOMContentLoaded", function () {
     const userListContainer = document.getElementById("users-list");
-    const paginationContainer = document.getElementById("pagination");
-    const searchInput = document.getElementById("search");
-    let selectedRows = [];
 
     // Fetch users from the API
-const apiUrl = 'https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json';
+    fetch('https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json/users')
+        .then(response => response.json())
+        .then(users => {
+            // Display users in the UI
+            users.forEach(user => {
+                const userCard = document.createElement('div');
+                userCard.className = 'user-card';
+                userCard.innerHTML = `
+                    <p><strong>ID:</strong> ${user.id}</p>
+                    <p><strong>Name:</strong> ${user.name}</p>
+                    <p><strong>Email:</strong> ${user.email}</p>
+                    <button class="delete-btn" onclick="deleteUser(${user.id})">Delete</button>
+                `;
+                userListContainer.appendChild(userCard);
+            });
+        })
+        .catch(error => console.error('Error fetching users:', error));
+});
 
-    // Variables to keep track of the state
-    let users = [];
-    let currentPage = 1;
-    let totalPages = 1;
-    let selectedRows = [];
-
-    // Function to fetch data from the API
-    async function fetchData() {
-      const response = await fetch(apiUrl);
-      const data = await response.json();
-      users = data;
-      renderTable();
-    }
-
-    // Function to render the table
-    function renderTable() {
-      const table = document.getElementById('userTable');
-      table.innerHTML = '';
-
-      // Add table headers
-      const headers = Object.keys(users[0]);
-      const headerRow = document.createElement('tr');
-      headers.forEach(header => {
-        const th = document.createElement('th');
-        th.textContent = header;
-        headerRow.appendChild(th);
-      });
-      table.appendChild(headerRow);
-
-      // Add table rows
-      const startIndex = (currentPage - 1) * 10;
-      const endIndex = startIndex + 10;
-      const rowsToDisplay = users.slice(startIndex, endIndex);
-
-      rowsToDisplay.forEach(rowData => {
-        const row = document.createElement('tr');
-        headers.forEach(header => {
-          const td = document.createElement('td');
-          td.textContent = rowData[header];
-          row.appendChild(td);
-        });
-
-        // Add edit and delete buttons to each row
-        const editButton = createButton('edit', 'Edit', () => editRow(row));
-        const deleteButton = createButton('delete', 'Delete', () => deleteRow(row));
-        row.appendChild(editButton);
-        row.appendChild(deleteButton);
-
-        table.appendChild(row);
-      });
-
-      updatePagination();
-    }
-
-    // Function to create action buttons
-    function createButton(className, text, onClick) {
-      const button = document.createElement('button');
-      button.className = className;
-      button.textContent = text;
-      button.addEventListener('click', onClick);
-      return button;
-    }
-
-    // Function to handle row editing
-    function editRow(row) {
-      // Implement row editing logic here
-      console.log('Editing row:', row);
-    }
-
-    // Function to handle row deletion
-    function deleteRow(row) {
-      // Implement row deletion logic here
-      console.log('Deleting row:', row);
-    }
-
-    // Function to handle selected rows
-    function handleRowSelection(row) {
-      const index = selectedRows.indexOf(row);
-      if (index === -1) {
-        selectedRows.push(row);
-      } else {
-        selectedRows.splice(index, 1);
-      }
-
-      renderTable();
-    }
-
-    // Function to delete selected rows
-    function deleteSelected() {
-      // Implement bulk delete logic here
-      console.log('Deleting selected rows:', selectedRows);
-      selectedRows = [];
-      renderTable();
-    }
-
-    // Function to handle pagination
-    function gotoPage(page) {
-      currentPage = page;
-      renderTable();
-    }
-
-    function prevPage() {
-      if (currentPage > 1) {
-        currentPage--;
-        renderTable();
-      }
-    }
-
-    function nextPage() {
-      if (currentPage < totalPages) {
-        currentPage++;
-        renderTable();
-      }
-    }
-
-    // Function to update pagination information
-    function updatePagination() {
-      const totalRecords = users.length;
-      totalPages = Math.ceil(totalRecords / 10);
-      document.getElementById('currentPage').textContent = `Page ${currentPage} of ${totalPages}`;
-    }
-
-    // Function to handle search
-    function search() {
-      const searchTerm = document.getElementById('search').value.toLowerCase();
-      if (searchTerm.trim() === '') {
-        fetchData();
-      } else {
-        const filteredUsers = users.filter(user =>
-          Object.values(user).some(value => value.toString().toLowerCase().includes(searchTerm))
-        );
-        currentPage = 1;
-        users = filteredUsers;
-        renderTable();
-      }
-    }
-
-    // Initial data fetch and rendering
-    fetchData();
+function deleteUser(userId) {
+    // Send a request to the API to delete the user
+    fetch(`https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json/users/${userId}`, {
+        method: 'DELETE',
+    })
+    .then(response => {
+        if (response.ok) {
+            // Remove the user card from the UI
+            const userCard = document.querySelector(`.user-card[data-id="${userId}"]`);
+            if (userCard) {
+                userCard.remove();
+            } else {
+                console.error('User card not found for deletion');
+            }
+        } else {
+            console.error('Error deleting user:', response.status);
+        }
+    })
+    .catch(error => console.error('Error deleting user:', error));
+}
