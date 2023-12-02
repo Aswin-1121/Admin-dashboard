ffae54b677ed7ff762bc0ad3c286cc690f639e60
Aswin-1121 <75619250+Aswin-1121@users.noreply.github.com>
2023-12-02 17:30:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,32 @@
-// server.js
-
 const express = require('express');
-const cors = require('cors');
 const app = express();
-const PORT = process.env.PORT || 3000;
-
-// Enable CORS for all routes
-app.use(cors());
+const PORT = 3000;
 
 // Dummy user data
-const users = require('https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json');
+const users = [
+    { id: 1, name: 'John Doe', email: 'john@example.com' },
+    { id: 2, name: 'Jane Doe', email: 'jane@example.com' },
+    // Add more users as needed
+];
+
+app.use(express.json());
 
 // Endpoint to get all users
 app.get('/users', (req, res) => {
     res.json(users);
+});
+
+// Endpoint to delete a user
+app.delete('/users/:userId', (req, res) => {
+    const userId = parseInt(req.params.userId);
+    const index = users.findIndex(user => user.id === userId);
+
+    if (index !== -1) {
+        users.splice(index, 1);
+        res.sendStatus(204); // No content
+    } else {
+        res.status(404).json({ error: 'User not found' });
+    }
 });
 
 app.listen(PORT, () => {
